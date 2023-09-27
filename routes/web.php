08826041642128c5ece6014c21b5211ef3810958
Jonas Baptista Franco <jonas@MacBook-Air-de-Jonas.local>
2023-09-27 06:59:41 -0300

    layout

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return ('Welcome');
+    // return ('Welcome');
+    return view('home');
 });
